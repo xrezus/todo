00bf8cf806acdb92a12c189d2056cba0ed12434a
Roman Nikiforov <rxafan@gmail.com>
2022-06-17 14:53:30 +0300

    Added DI

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/xrezus/todo"
 	"github.com/xrezus/todo/pkg/handler"
+	"github.com/xrezus/todo/pkg/repository"
+	"github.com/xrezus/todo/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xrezus/todo/pkg/service"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{services: service}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
